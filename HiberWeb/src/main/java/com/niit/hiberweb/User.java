a564bd3ae24a0a2e656a8fab7c4547124c7234c8
Training <Training@192.168.252.215>
2020-03-13 12:15:24 +0530

    //main

---
+++ HiberWeb/src/main/java/com/niit/hiberweb/User.java
@@ -0,0 +1,37 @@
+package com.niit.hiberweb;
+
+public class User {
+	private int id;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	private String name;
+	private String password;
+	private String email;
+
+}
